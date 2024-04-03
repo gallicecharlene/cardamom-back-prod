0044ad03c005b5de52c6f79d9938f94dc26a44ce
gallicecharlene <charlene.gallice@oclock.school>
2024-04-03 11:49:13 +0200

    token generation

--- app/router.js
+++ app/router.js
@@ -1,9 +1,30 @@
 import { Router } from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = Router();
 
 router.get('/', (req, res) => {
     res.send('Hello World');
+});
+router.get('/jwt', (req, res) => {
+    const createToken = (jsonData = {}) => {
+        try {
+            const secretKey = 'test';
+            const token = jwt.sign(jsonData, secretKey);
+            return token;
+        } catch (error) {
+            console.log('error', error.message);
+            return null;
+        }
+    };
+
+    const jsonData = { email: 'test@test.fr', password: 'test1234' }; // req.body
+    const token = createToken(jsonData);
+
+    if (token) {
+        res.json({ status: true, token });
+    }
+    res.json({ status: false });
 });
 
 export default router;
