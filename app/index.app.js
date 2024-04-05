5faca0daccf2792dd4fd48bc59f3d7c5251c83b7
Thomas-Laronde <laronde.chevaillier@gmail.com>
2024-04-05 15:31:04 +0200

    feat: Adding CORS to run api on front

--- app/index.app.js
+++ app/index.app.js
@@ -1,10 +1,15 @@
 import express from 'express';
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+import cors from 'cors';
 import router from './router.js';
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors({
+    // origin: '*',
+}));
 app.use(router);
 
 export default app;
