d380af7102fdfc919ea96e627efd05b325217c27
FranzEricPige <francois.pglt@gmail.com>
2024-04-10 15:06:25 +0200

    fix: add  middleware authJwt to all not public routes

--- app/router.js
+++ app/router.js
@@ -10,21 +10,21 @@ const router = Router();
 // route profile avec vérification du token
 
 router.get('/api/decks/test'); // Récupération du deck d'essai (deck de test)
-router.get('/api/decks', deckController.getAll); // Récupérer tous les decks du User
-router.get('/api/decks/:deckId', deckController.getOne); // Récupérer un deck spécifique via son ID + récup toutes ses flashcards
-router.post('/api/decks', deckController.create); // Créer un deck
-router.patch('/api/decks/:deckId', deckController.update); // Mettre à jour un deck
-router.delete('/api/decks/:deckId', deckController.delete); // Supprimer un deck spécifique
+router.get('/api/decks', authJwt, deckController.getAll); // Récupérer tous les decks du User
+router.get('/api/decks/:deckId', authJwt, deckController.getOne); // Récupérer un deck spécifique via son ID + récup toutes ses flashcards
+router.post('/api/decks', authJwt, deckController.create); // Créer un deck
+router.patch('/api/decks/:deckId', authJwt, deckController.update); // Mettre à jour un deck
+router.delete('/api/decks/:deckId', authJwt, deckController.delete); // Supprimer un deck spécifique
 
-router.post('/api/flashcards', flashcardController.create); // Créer une flashcard
-router.patch('/api/flashcards/:flashcardId', flashcardController.update); // Modifier une flashcard depuis son ID (DECK ID OBLIGATOIRE)
-router.delete('/api/flashcards/:flashcardId', flashcardController.delete); // Supprimer une flashcard spécifique
+router.post('/api/flashcards', authJwt, flashcardController.create); // Créer une flashcard
+router.patch('/api/flashcards/:flashcardId', authJwt, flashcardController.update); // Modifier une flashcard depuis son ID (DECK ID OBLIGATOIRE)
+router.delete('/api/flashcards/:flashcardId', authJwt, flashcardController.delete); // Supprimer une flashcard spécifique
 
-router.get('/api/stats', statsController.getAll); // Récupérer les stats de tous les decks confondus
-router.get('/api/decks/:deckId/stats', statsController.getOne); // Récupérer les stats d'un deck
-router.get('/api/decks/:deckId/stats', statsController.create); // initialiser un MemoTest (on associe une nouvelle stat à un deck)
-router.patch('/api/decks/:deckId/stats', statsController.update); // route d'incrémentation depuis la page MemoTest
-router.patch('/api/decks/:deckId/stats', statsController.delete); // éventuellement possibilité de supprimer les stats d'un deck depuis le deckId
+router.get('/api/stats', authJwt, statsController.getAll); // Récupérer les stats de tous les decks confondus
+router.get('/api/decks/:deckId/stats', authJwt, statsController.getOne); // Récupérer les stats d'un deck
+router.get('/api/decks/:deckId/stats', authJwt, statsController.create); // initialiser un MemoTest (on associe une nouvelle stat à un deck)
+router.patch('/api/decks/:deckId/stats', authJwt, statsController.update); // route d'incrémentation depuis la page MemoTest
+router.patch('/api/decks/:deckId/stats', authJwt, statsController.delete); // éventuellement possibilité de supprimer les stats d'un deck depuis le deckId
 
 // route login et génération du token
 router.post('/api/auth/login', authController.login);
