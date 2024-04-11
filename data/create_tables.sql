4477f09c1600a234f9327367d71e81ae5814663c
FranzEricPige <francois.pglt@gmail.com>
2024-04-11 10:52:52 +0200

    fix: add constraints not null in sql script for user_id in deck and deck_id in flashcard

--- data/create_tables.sql
+++ data/create_tables.sql
@@ -11,7 +11,7 @@ CREATE TABLE "user" (
 CREATE TABLE "deck" (
     "id" INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     "title" VARCHAR(128) NOT NULL,
-    "user_id" INTEGER REFERENCES "user"("id"),
+    "user_id" INTEGER REFERENCES NOT NULL "user"("id"),
     "share_id" VARCHAR(64),  -- NOT NULL & UNIQUE sont retirés pour le test de création de deck
     "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMPTZ
@@ -21,7 +21,7 @@ CREATE TABLE "flashcard" (
     "id" INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     "title_front" VARCHAR NOT NULL,
     "title_back" VARCHAR NOT NULL,
-    "deck_id" INTEGER REFERENCES "deck"("id"),
+    "deck_id" INTEGER REFERENCES NOT NULL "deck"("id"),
     "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMPTZ
 );
