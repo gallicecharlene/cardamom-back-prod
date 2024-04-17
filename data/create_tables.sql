528128c3f431f3caa2da4155e3ec774a745c8c32
gallicecharlene <charlene.gallice@oclock.school>
2024-04-17 10:37:10 +0200

    fix: Changing the attribute's value suppression behaviour with ON DELETE CASCADE

--- data/create_tables.sql
+++ data/create_tables.sql
@@ -11,7 +11,7 @@ CREATE TABLE "user" (
 CREATE TABLE "deck" (
     "id" INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     "title" VARCHAR(128) NOT NULL,
-    "user_id" INTEGER NOT NULL REFERENCES "user"("id"),
+    "user_id" INTEGER NOT NULL REFERENCES "user"("id")ON DELETE CASCADE,
     "share_id" VARCHAR(64) NOT NULL,  
     "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMPTZ
@@ -21,15 +21,15 @@ CREATE TABLE "flashcard" (
     "id" INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     "title_front" VARCHAR NOT NULL,
     "title_back" VARCHAR NOT NULL,
-    "deck_id" INTEGER NOT NULL REFERENCES "deck"("id"),
+    "deck_id" INTEGER NOT NULL REFERENCES "deck"("id") ON DELETE CASCADE,
     "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMPTZ
 );
 
 CREATE TABLE "deck_has_user" (
     "id" INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    "user_id" INTEGER NOT NULL REFERENCES "user"("id"),
-    "deck_id" INTEGER NOT NULL REFERENCES "deck"("id"),
+    "user_id" INTEGER NOT NULL REFERENCES "user"("id") ON DELETE CASCADE,
+    "deck_id" INTEGER NOT NULL REFERENCES "deck"("id") ON DELETE CASCADE,
     "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMPTZ
 );
@@ -39,8 +39,8 @@ CREATE TABLE "stats" (
     "date" DATE,
     "nb_card_consulted" INTEGER DEFAULT 0,
     "nb_card_success" INTEGER DEFAULT 0,
-    "user_id" INTEGER NOT NULL REFERENCES "user"("id"),
-    "deck_id" INTEGER NOT NULL REFERENCES "deck"("id"),
+    "user_id" INTEGER NOT NULL REFERENCES "user"("id") ON DELETE CASCADE,
+    "deck_id" INTEGER NOT NULL REFERENCES "deck"("id") ON DELETE CASCADE,
     "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMPTZ
 );
