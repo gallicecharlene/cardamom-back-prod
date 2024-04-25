c1f8fc8e9273011e955e6f41ae58080f3bf78c7c
gallicecharlene <charlene.gallice@oclock.school>
2024-04-26 00:07:23 +0200

    Supprimer la limite de taux d'appel

--- app/index.app.js
+++ app/index.app.js
@@ -1,21 +1,8 @@
 import express from 'express';
 import cors from 'cors';
-import rateLimit from 'express-rate-limit';
 import router from './router.js';
 
 const app = express();
-
-// Définir trust proxy à true
-app.set('trust proxy', true);
-
-// Add rate limit policy
-const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100000, // Limit each IP to 100K requests per `window` (here, per 15 minutes)
-    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-});
-app.use(limiter);
 
 app.use(cors());
 
