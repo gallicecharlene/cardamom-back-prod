0504a962e08fd1881c86631e0e7e07c6ce16abcf
gallicecharlene <charlene.gallice@oclock.school>
2024-04-26 12:39:58 +0200

    ✨feature: add errorHandler for stats

--- app/router.js
+++ app/router.js
@@ -22,12 +22,12 @@ router.post('/api/flashcards', authJwt, errorHandler(flashcardController.create)
 router.patch('/api/flashcards/:flashcardId', authJwt, errorHandler(flashcardController.update)); // Modifier une flashcard depuis son ID (DECK ID OBLIGATOIRE)
 router.delete('/api/flashcards/:flashcardId', authJwt, errorHandler(flashcardController.delete)); // Supprimer une flashcard spécifique
 
-router.get('/api/stats', authJwt, statsController.getAll); // Récupérer les stats de tous les decks confondus pour l'utilisateur
-router.get('/api/stats/:statsId', authJwt, statsController.getOne); // Récupérer la stats par son Id (par utilisateur)
-router.patch('/api/stats/:statsId', authJwt, statsController.update); // route d'incrémentation depuis la page MemoTest
-router.delete('/api/stats/:statsId', authJwt, statsController.delete); // supprimer une stats par son Id (par utilisateur)
-router.get('/api/decks/:deckId/stats', authJwt, statsController.getByDeck); // Récupérer les stats d'un deck
-router.post('/api/decks/:deckId/stats', authJwt, statsController.create); // initialiser un MemoTest (on associe une nouvelle stat à un deck)
+router.get('/api/stats', authJwt, errorHandler(statsController.getAll)); // Récupérer les stats de tous les decks confondus pour l'utilisateur
+router.get('/api/stats/:statsId', authJwt, errorHandler(statsController.getOne)); // Récupérer la stats par son Id (par utilisateur)
+router.patch('/api/stats/:statsId', authJwt, errorHandler(statsController.update)); // route d'incrémentation depuis la page MemoTest
+router.delete('/api/stats/:statsId', authJwt, errorHandler(statsController.delete)); // supprimer une stats par son Id (par utilisateur)
+router.get('/api/decks/:deckId/stats', authJwt, errorHandler(statsController.getByDeck)); // Récupérer les stats d'un deck
+router.post('/api/decks/:deckId/stats', authJwt, errorHandler(statsController.create)); // initialiser un MemoTest (on associe une nouvelle stat à un deck)
 
 // route login et génération du token
 router.post('/api/auth/login', errorHandler(authController.login));
